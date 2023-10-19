3847796d5bbeb71e0831ff9ea9e99cd6c10496b7
askarhabibulloh <mhmmdaskrhbblh@gmail.com>
2023-10-19 11:05:49 +0700

    11 Oktober 2023 - Cuma Hello World

---
+++ 1/1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  1
+//
+//  Created by Muhammad Askar Habibulloh on 11/10/23.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!\nNama : Muhammad Askar H\nNIM : 22106050031";
+    cin.get();
+    return 0;
+}
